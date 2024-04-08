c3344f15a3e987034be92a7e4c678bd27605beaf
Pravin12111 <pravinchavan0656@gmail.com>
2024-04-09 01:01:19 +0530

    Login page create with logo of spotify and login with spotify button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
-
+import Login from './Login'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>SPOTIFY</h1>
-      </header>
+        <Login/>
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,7 @@
+//https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+const authend = "https://accounts.spotify.com/authorize";
+
+const redirectul = "http://localhost:3000/";
+
+const clientid = ""
