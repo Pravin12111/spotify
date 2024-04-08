a2f81ea7b1de012b4788f12155b1ac0c7cca70a4
Pravin12111 <pravinchavan0656@gmail.com>
2024-04-09 02:02:57 +0530

    User Authentication added to Login page

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,18 @@
 //https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
 
-const authend = "https://accounts.spotify.com/authorize";
+const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectul = "http://localhost:3000/";
+const redirectUrl = "http://localhost:3000/";
 
-const clientid = ""
+const clientId = "a967e1837fb5495897324b64d1f50b55"
+
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
