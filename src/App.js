ac0cb43dd187ebbc27403236656b2b9d33f33f2f
Pravin12111 <pravinchavan0656@gmail.com>
2024-04-09 12:01:18 +0530

    get user information by spotifywebapi

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login'
 import { getTokenFromUrl } from './spotify';
+import spotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify = new spotifyWebApi();
+
 function App() {
   //run code based on a given condition
   const [token,setToken] =useState(null);
@@ -15,6 +20,11 @@ function App() {
 
     if(_token){
       setToken(_token)
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
     }
     console.log('I have a token: ',token);
   },[]);
@@ -22,7 +32,7 @@ function App() {
     <div className="App">
       {
         token ?
-        (<h1>I am Logged in</h1>)
+        (<Player/>)
         :
         (
           <Login/>
