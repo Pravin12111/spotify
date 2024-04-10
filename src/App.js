528b04ff20d061c1b3397bbbed7bce930b8152cf
Pravin12111 <pravinchavan0656@gmail.com>
2024-04-10 21:34:36 +0530

    start designing spotify landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
-import { useEffect, useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login'
 import { getTokenFromUrl } from './spotify';
 import spotifyWebApi from 'spotify-web-api-js';
 import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new spotifyWebApi();
 
 function App() {
   //run code based on a given condition
-  const [token,setToken] =useState(null);
+  //const [token,setToken] =useState(null);
+  const [{ user ,token},dispatch]= useDataLayerValue();
+
   useEffect(()=>{
     //code...
 
@@ -19,25 +22,37 @@ function App() {
     const _token =hash.access_token;
 
     if(_token){
-      setToken(_token)
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+      //setToken(_token)
 
       spotify.setAccessToken(_token);
       spotify.getMe().then((user) => {
-        console.log(user);
+        //console.log(user);
+
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+
+        });
       });
     }
     console.log('I have a token: ',token);
   },[]);
+
+  console.log('I am',user);
+  console.log('My token:',token);
+
   return (
     <div className="App">
       {
         token ?
-        (<Player/>)
+          <Player spotify={spotify}/>
         :
-        (
           <Login/>
-        )
-
       }
     </div>
   );
---
+++ src/reducer.js
@@ -0,0 +1,28 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    //remove after finished developing
+    token: 'BQDsmeXh1mI4spkmM-JswZfPHIDvwT_wGYJ1XojImF285NJWB0XxggNOQwTGEB2JFQFdUDZaDY8t83DDtSK8CLWF8l0lttqYCOJqbqx1oMrWqotvHlNP4D9vl4xkZYuwAsm-ZJDE6XkJltyOc2rEtssA02ZYGuLQElXbV5Lxyo2-wntihUTTW3c3m2TyvvMI173tQjU6XW0jTPk4L4uH'
+};
+
+export const reducer = (state,action)=>{
+    console.log(action);
+
+    switch(action.type){
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            }
+
+            case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            }
+            default:
+                return state
+    }
+}
