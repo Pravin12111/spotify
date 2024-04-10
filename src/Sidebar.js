01d324fcb9678b9ec2b00f611152297ec0346f40
Pravin12111 <pravinchavan0656@gmail.com>
2024-04-10 22:39:54 +0530

    spotify sidebar is created by using material ui

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./Sidebar.css"
-
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from "@mui/icons-material/Search"
+import LibraryIcon from "@mui/icons-material/LibraryMusic"
 function Sidebar() {
 return (
     <div className='sidebar'>
         <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" className='sidebar-logo'/>
-    </div>
+
+        <SidebarOption title="Home" Icon={HomeIcon}/>
+        <SidebarOption title="Search"   Icon={SearchIcon}/>
+        <SidebarOption title="Your Libraray"    Icon={LibraryIcon}/>
+
+        <br/>
+        <strong className='sidebar-title'>PLAYLISTS</strong>
+        <hr/>
+    </div>  
 )
 }
 
